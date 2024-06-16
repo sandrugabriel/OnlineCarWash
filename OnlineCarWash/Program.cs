0c0bce7c637dee6d1dc9ff7f621cd7c22dd4d3b1
sandrugabriel <sandru11.gabriel@gmail.com>
2024-06-16 14:46:30 +0300

    done

--- OnlineCarWash/Program.cs
+++ OnlineCarWash/Program.cs
@@ -18,60 +18,66 @@ using OnlineCarWash.Services.Repository.interfaces;
 using OnlineCarWash.Services.ServiceCommandQuery;
 using OnlineCarWash.Services.ServiceCommandQuery.interfaces;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IRepositoryCustomer, RepositoryCustomer>();
-builder.Services.AddScoped<ICommandServiceCustomer, CommandServiceCustomer>();
-builder.Services.AddScoped<IQueryServiceCustomer, QueryServiceCustomer>();
-
-builder.Services.AddScoped<IRepositoryOption, RepositoryOption>();
-builder.Services.AddScoped<IOptionCommandService, OptionCommandService>();
-builder.Services.AddScoped<IOptionQueryService, OptionQueryService>();
-
-builder.Services.AddScoped<IRepositoryService, RepositoryService>();
-builder.Services.AddScoped<IServiceQueryService, ServiceQueryService>();
-builder.Services.AddScoped<IServiceCommandService, ServiceCommandService>();
-
-builder.Services.AddScoped<IRepositoryAppointment, RepositoryAppointment>();
-builder.Services.AddScoped<IAppointmentQueryService, AppointmentQueryService>();
-
-
-builder.Services.AddDbContext<AppDbContext>(op => op.UseMySql(builder.Configuration.GetConnectionString("Default")!,
-    new MySqlServerVersion(new Version(8, 0, 21))), ServiceLifetime.Scoped);
-
-builder.Services.AddFluentMigratorCore()
-    .ConfigureRunner(rb => rb.AddMySql5().WithGlobalConnectionString(builder.Configuration.GetConnectionString("Default"))
-    .ScanIn(typeof(Program).Assembly).For.Migrations())
-    .AddLogging(lb => lb.AddFluentMigratorConsole());
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<IRepositoryCustomer, RepositoryCustomer>();
+        builder.Services.AddScoped<ICommandServiceCustomer, CommandServiceCustomer>();
+        builder.Services.AddScoped<IQueryServiceCustomer, QueryServiceCustomer>();
+
+        builder.Services.AddScoped<IRepositoryOption, RepositoryOption>();
+        builder.Services.AddScoped<IOptionCommandService, OptionCommandService>();
+        builder.Services.AddScoped<IOptionQueryService, OptionQueryService>();
+
+        builder.Services.AddScoped<IRepositoryService, RepositoryService>();
+        builder.Services.AddScoped<IServiceQueryService, ServiceQueryService>();
+        builder.Services.AddScoped<IServiceCommandService, ServiceCommandService>();
+
+        builder.Services.AddScoped<IRepositoryAppointment, RepositoryAppointment>();
+        builder.Services.AddScoped<IAppointmentQueryService, AppointmentQueryService>();
+
+
+        builder.Services.AddDbContext<AppDbContext>(op => op.UseMySql(builder.Configuration.GetConnectionString("Default")!,
+            new MySqlServerVersion(new Version(8, 0, 21))), ServiceLifetime.Scoped);
+
+        builder.Services.AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb.AddMySql5().WithGlobalConnectionString(builder.Configuration.GetConnectionString("Default"))
+            .ScanIn(typeof(Program).Assembly).For.Migrations())
+            .AddLogging(lb => lb.AddFluentMigratorConsole());
+
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-    runner.MigrateUp();
-}
-
-app.Run();
